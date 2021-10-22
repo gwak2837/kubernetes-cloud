ec8b0b9f82c8e3a6b0519006e06f68ba595a0e58
Taeuk Gwak <gwak2837@kakao.com>
2021-10-22 18:43:15 +0900

    Initialize project

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  poweredByHeader: process.env.NODE_ENV === 'development',
+  reactStrictMode: process.env.NODE_ENV === 'development',
+}
