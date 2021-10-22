9496d4090202794141df647059b01853a3be5770
Taeuk Gwak <gwak2837@kakao.com>
2021-10-22 18:43:20 +0900

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Navigation from 'src/components/Navigation'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Home
