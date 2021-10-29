f82fd218cee17c69bd28abed1c4ef1c91ac5bd2b
Taeuk Gwak <gwak2837@kakao.com>
2021-10-29 17:15:28 +0900

    Add console log

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,16 @@ import 'normalize.css'
 import 'src/styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    console.log('👀 - process.env.NEXT_PUBLIC_BACKEND_URL', process.env.NEXT_PUBLIC_BACKEND_URL)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
