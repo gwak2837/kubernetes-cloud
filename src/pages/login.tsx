b8807ae92e63dd520e9dfb377f62b147d097f6b6
Taeuk Gwak <gwak2837@kakao.com>
2021-11-02 12:18:07 +0900

    Update styles on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,18 +1,108 @@
 import { Button, Input } from 'antd'
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { FormEvent, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { useMutation } from 'react-query'
+import Checkbox from 'src/components/Checkbox'
 import Navigation from 'src/components/Navigation'
+import EmailIcon from 'src/svgs/EmailIcon'
+import PasswordIcon from 'src/svgs/PasswordIcon'
+import SobokLogo from 'src/svgs/sobok-logo.svg'
 import styled from 'styled-components'
 
 import { digestMessageWithSHA256 } from '../utils'
 import { GridContainerForm, Label, RedText, validateEmail, validatePassword } from './register'
 
+const Padding = styled.div`
+  padding: 1rem;
+  background: #fcfcfc;
+  height: 100vh;
+  max-width: 400px;
+  margin: 0 auto;
+`
+
+const Padding1 = styled.div`
+  padding: 1rem 0;
+  text-align: right;
+`
+
+const Padding4 = styled.div`
+  padding-top: 4rem;
+  text-align: right;
+`
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 1fr 1px 1fr;
+  place-items: center center;
+  gap: 0.5rem 0;
+
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  border: solid 1px #e0e0e0;
+  background: #fff;
+`
+
+const LogoWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr 1fr 0.5fr;
+
+  svg {
+    grid-column: 2 / 3;
+    grid-row: 2 / 3;
+    width: 100%; // for safari
+    cursor: pointer;
+  }
+`
+
+const BlackText = styled.span`
+  color: black;
+  cursor: pointer;
+  padding: 0.5rem;
+`
+
+const EmailIconWraptper = styled.div`
+  width: 1.5rem;
+
+  svg {
+    display: block;
+    margin: auto;
+  }
+`
+
+const PasswordIconWraptper = styled.div`
+  width: 1rem;
+
+  svg {
+    display: block;
+    margin: auto;
+  }
+`
+
+const HorizontalLine = styled.div`
+  width: 100%;
+
+  grid-column: 1 / 3;
+  border-top: solid 1px #e0e0e0;
+`
+
+const StyledButton = styled(Button)`
+  height: 3.5rem;
+  width: 100%;
+`
+
+const CenterText = styled.div`
+  text-align: center;
+`
+
 type LoginFormValues = {
   email: string
   password: string
+  remember: boolean
 }
 
 async function loginRequest(userInfo: Record<string, string>) {
@@ -34,13 +124,14 @@ const LoginPage: NextPage = () => {
     control,
     formState: { errors },
     handleSubmit,
+    watch,
   } = useForm<LoginFormValues>({
-    defaultValues: { email: '', password: '' },
+    defaultValues: { email: '', password: '', remember: false },
   })
 
   const router = useRouter()
 
-  const mutation = useMutation(loginRequest, {
+  const { mutate, isLoading } = useMutation(loginRequest, {
     onError: (error) => {
       alert(error)
     },
@@ -56,35 +147,41 @@ const LoginPage: NextPage = () => {
 
   async function login({ email, password }: LoginFormValues) {
     const passwordHash = await digestMessageWithSHA256(password)
-    mutation.mutate({ email, passwordHash })
+    mutate({ email, passwordHash })
   }
 
   return (
-    <>
-      <Navigation />
+    <Padding>
+      <LogoWrapper>
+        <SobokLogo onClick={() => router.push('/')} />
+      </LogoWrapper>
 
-      <GridContainerForm onSubmit={handleSubmit(login)}>
-        <label>
-          <Label>이메일</Label>
+      <form onSubmit={handleSubmit(login)}>
+        <GridContainer>
+          <EmailIconWraptper>
+            <EmailIcon colored={Boolean(watch('email'))} />
+          </EmailIconWraptper>
           <Controller
             control={control}
             name="email"
             render={({ field }) => (
-              <Input placeholder="이메일을 입력해주세요" size="large" type="email" {...field} />
+              <Input bordered={false} placeholder="이메일을 입력해주세요" size="large" {...field} />
             )}
             rules={validateEmail}
           />
-          {errors.email && <RedText>{errors.email.message}</RedText>}
-        </label>
 
-        <label>
-          <Label>비밀번호</Label>
+          <HorizontalLine />
+
+          <PasswordIconWraptper>
+            <PasswordIcon colored={Boolean(watch('password'))} />
+          </PasswordIconWraptper>
           <Controller
             control={control}
             name="password"
             render={({ field }) => (
               <Input
-                placeholder="비밀번호를 입력해주세요"
+                bordered={false}
+                placeholder="쿠버네티스 마스터가 되자"
                 size="large"
                 type="password"
                 {...field}
@@ -92,14 +189,48 @@ const LoginPage: NextPage = () => {
             )}
             rules={validatePassword}
           />
-          {errors.password && <RedText>{errors.password.message}</RedText>}
-        </label>
+        </GridContainer>
 
-        <Button htmlType="submit" size="large" type="primary">
+        <RedText>{errors.email?.message || errors.password?.message}</RedText>
+
+        <Padding1>
+          <Controller
+            control={control}
+            name="remember"
+            render={({ field }) => (
+              <Checkbox checked={field.value} disabled={isLoading} {...field}>
+                로그인 유지
+              </Checkbox>
+            )}
+          />
+        </Padding1>
+
+        <StyledButton loading={isLoading} htmlType="submit" size="large" type="primary">
           로그인
-        </Button>
-      </GridContainerForm>
-    </>
+        </StyledButton>
+
+        <Padding4 />
+
+        <StyledButton loading={isLoading} htmlType="submit" size="large">
+          간편 로그인
+        </StyledButton>
+
+        <Padding1 />
+
+        <CenterText>
+          <Link href="/register" passHref>
+            <a>
+              <BlackText>회원가입</BlackText>
+            </a>
+          </Link>
+          <Link href="/register" passHref>
+            <a>
+              <BlackText>아이디/비밀번호 찾기</BlackText>
+            </a>
+          </Link>
+        </CenterText>
+      </form>
+    </Padding>
   )
 }
 
