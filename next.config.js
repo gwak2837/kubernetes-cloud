2c2963a7ed3de98bdaf9eb9bb3baf364d001c603
Taeuk Gwak <gwak2837@kakao.com>
2021-11-04 00:22:41 +0900

    Add next pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
   poweredByHeader: process.env.NODE_ENV === 'development',
   reactStrictMode: process.env.NODE_ENV === 'development',
   webpack: (config) => {
@@ -9,4 +15,4 @@ module.exports = {
     })
     return config
   },
-}
+})
