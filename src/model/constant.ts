06421b0372db0bf1f463b14c4c65858ad569ee44
Taeuk Gwak <gwak2837@kakao.com>
2021-11-04 00:22:59 +0900

    Add html head

--- src/model/constant.ts
+++ src/model/constant.ts
@@ -1,3 +1,12 @@
 export const MOBILE_MIN_WIDTH = '280px'
 export const TABLET_MIN_WIDTH = '560px'
 export const DESKTOP_MIN_WIDTH = '1024px'
+
+export const PRIMARY_BACKGROUND_COLOR = '#326ce5' // manifest.json 파일의 theme_color 필드랑 일치
+
+export const APPLICATION_SHORT_NAME = '쿠버네티스'
+export const APPLICATION_NAME = '쿠버네티스 클라우드 최종 프로젝트'
+export const CANONICAL_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    : 'http://localhost:3000'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import 'src/styles/globals.css'
 
 import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ReactElement, ReactNode, useEffect } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
@@ -24,11 +25,16 @@ function KubernetesCloudApp({ Component, pageProps }: AppPropsWithLayout) {
   }, [])
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        {getLayout ? getLayout(<Component {...pageProps} />) : <Component {...pageProps} />}
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          {getLayout ? getLayout(<Component {...pageProps} />) : <Component {...pageProps} />}
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   )
 }
 
